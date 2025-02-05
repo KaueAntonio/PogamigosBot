bfcdf78a80ef195114385308822276e09e86a8ee
KaueAntonio <kaue4991@gmail.com>
2025-02-05 19:25:03 -0300

    feat: add envs for appsettings

--- DiscordBot.Worker/Program.cs
+++ DiscordBot.Worker/Program.cs
@@ -2,6 +2,8 @@ using DiscordBot.Worker.Configurations;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables();
+
 builder.Services.AddBotModules();
 
 var host = builder.Build();
